ce0e267a92bbdb5a262dbe9ef7e25c7df297e989
BIHEUL Yannick <yannick.biheul@uhc-sante.fr>
2023-01-02 13:31:10 +0100

    Ajout css et js

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const champ = document.querySelector('#champ');
+const result = document.querySelector('#result');
+
+result.style.display = "none";
+
+champ.addEventListener("keyup", () => {
+    console.log('coucou');
+})
